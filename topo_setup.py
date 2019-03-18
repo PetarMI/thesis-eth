86288c1663ae43e72d1523c74dea3a4201ebd4c7
PetarMI <petar2394@gmail.com>
2019-03-18 18:19:23 +0100

    Docker-compose add volumes

--- topo_setup.py
+++ topo_setup.py
@@ -5,17 +5,80 @@ import constants as const
 import topo_parser as tp
 
 
-def main(topology_file: str):
-    topo: dict = import_topo(topology_file)
-    topo_meta = tp.find_metadata(topo)
-    frr_containers: list = tp.find_containers(topo, "frr")
-    topo_nets: list = tp.find_nets(topo)
+class ComposeGenerator():
 
-    topo_compose: dict = {}
-    write_version(topo_meta, topo_compose)
-    write_containers(frr_containers, topo_compose)
-    write_nets(topo_nets, topo_compose)
-    write_docker_compose(topo_meta, topo_compose)
+    def __init__(self, topology_file: str):
+        self.topo: dict = import_topo(topology_file)
+        self.topo_meta = tp.find_metadata(self.topo)
+        self.frr_containers: list = tp.find_containers(self.topo, "frr")
+        self.topo_nets: list = tp.find_nets(self.topo)
+
+    def compose(self):
+        topo_compose: dict = {}
+        self.write_version(topo_compose)
+        self.write_frr_containers(topo_compose)
+        self.write_nets(topo_compose)
+        self.write_docker_compose(topo_compose)
+
+    def write_version(self, topo_compose: dict) -> dict:
+        version = self.topo_meta.get("version", "3.7")
+
+        topo_compose.update({"version": version})
+
+        return topo_compose
+
+    def write_frr_containers(self, topo_compose: dict) -> dict:
+        containers = {}
+
+        for c in self.frr_containers:
+            c_ifaces = write_container_ifaces(c.get("interfaces"))
+            c_name = self.topo_meta.get("name") + "-" + c.get("name")
+            c_volumes = write_container_volumes()
+
+            containers.update({
+                c.get("name"): {
+                    "image": "phynet:0.1",
+                    "container_name": c_name,
+                    "privileged": True,
+                    "volumes": c_volumes,
+                    "networks": c_ifaces
+                }
+            })
+
+        topo_compose.update({"services": containers})
+
+        return topo_compose
+
+    def write_nets(self, topo_compose: dict) -> dict:
+        nets = {}
+
+        for net in self.topo_nets:
+            nets.update({
+                net.get("name"): {
+                    "driver": "bridge",
+                    "attachable": True,
+                    "ipam": {
+                        "driver": "default",
+                        "config": [
+                            {
+                                "subnet": net.get("subnet")
+                            }
+                        ]
+                    }
+                }
+            })
+
+        topo_compose.update({"networks": nets})
+
+        return topo_compose
+
+    def write_docker_compose(self, topo_compose: dict):
+        topo_name = self.topo_meta.get("name")
+        compose_file = const.TOPO_DIR + topo_name + "/" + const.DOCKER_COMPOSE
+
+        with open(compose_file, 'w') as docker_compose:
+            yaml.dump(topo_compose, docker_compose,
+                      default_flow_style=False, sort_keys=False)
 
 
 def import_topo(topo_name: str) -> dict:
@@ -28,76 +91,27 @@ def import_topo(topo_name: str) -> dict:
     return topo
 
 
-def write_version(topo_meta: dict, topo_compose: dict) -> dict:
-    version = topo_meta.get("version", "3.7")
-
-    topo_compose.update({"version": version})
-
-    return topo_compose
-
-
-def write_containers(topo_containers: list, topo_compose: dict) -> dict:
-    containers = {}
-
-    for c in topo_containers:
-        c_ifaces = write_container_ifaces(c.get("interfaces"))
-
-        containers.update({
-            c.get("name"): {
-                "image": "docker:dind",
-                # "container_name": c.get("name"),
-                "privileged": True,
-                "networks": c_ifaces
-            }
-        })
-
-    topo_compose.update({"services": containers})
-
-    return topo_compose
-
-
 def write_container_ifaces(cont_interfaces: list) -> dict:
     ifaces = {}
 
     for iface in cont_interfaces:
-        ifaces.update(
-            {iface.get("network"): {
+        ifaces.update({
+            iface.get("network"): {
                 "ipv4_address": iface.get("ipaddr")
-            }})
+            }
+        })
 
     return ifaces
 
 
-def write_nets(topo_nets: list, topo_compose: dict) -> dict:
-    nets = {}
+def write_container_volumes() -> list:
+    volumes = [{
+        "type": "bind",
+        "source": "./configs",
+        "target": "/home/configs"
+    }]
 
-    for net in topo_nets:
-        nets.update(
-            {net.get("name"): {
-                "driver": "bridge",
-                "attachable": True,
-                "ipam": {
-                    "driver": "default",
-                    "config": [
-                        {
-                            "subnet": net.get("subnet")
-                        }
-                    ]
-                }
-            }})
-
-    topo_compose.update({"networks": nets})
-
-    return topo_compose
-
-
-def write_docker_compose(topo_meta: dict, topo_compose: dict):
-    topo_name = topo_meta.get("name")
-    compose_file = const.TOPO_DIR + topo_name + "/" + const.DOCKER_COMPOSE
-
-    with open(compose_file, 'w') as docker_compose:
-        yaml.dump(topo_compose, docker_compose,
-                  default_flow_style=False, sort_keys=False)
+    return volumes
 
 
 if __name__ == '__main__':
@@ -107,4 +121,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     topo_name: str = args.filename
-    main(topo_name)
+
+    composer = ComposeGenerator(topo_name)
+    composer.compose()
