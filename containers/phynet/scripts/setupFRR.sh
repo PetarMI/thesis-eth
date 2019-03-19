0a683016cc0c6eb88d670e02eaecba5ac2946fda
PetarMI <petar2394@gmail.com>
2019-03-19 17:18:28 +0100

    Phynet container chooses the correct config file

--- containers/phynet/scripts/setupFRR.sh
+++ containers/phynet/scripts/setupFRR.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-echo "1/2 Pulling FRR image"
+echo "1/5 Pulling FRR image"
 docker pull petarmi/frr:6.0.2
 
-echo "2/2 Setting up FRR container"
+echo "2/5 Setting up FRR container"
 docker run -itd --privileged --name frr --network host petarmi/frr:6.0.2
+
+echo "3/5 Copying config files to FRR container"
+config_file=$(hostname)
+docker cp /home/configs/$config_file.conf frr:/home/device-config.conf
+
+echo "4/5 Setting configs to frr.conf"
+docker exec frr bash -c "cp /home/device-config.conf /etc/frr/frr.conf"
+
+echo "5/5 Restart FRR container to load new configs"
+docker restart frr
