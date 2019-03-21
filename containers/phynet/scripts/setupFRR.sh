18d1868213e395f8389818dab38ae48dd197fb62
PetarMI <petar2394@gmail.com>
2019-03-21 18:13:19 +0100

    Parallelize Layer 2 container setup

--- containers/phynet/scripts/setupFRR.sh
+++ containers/phynet/scripts/setupFRR.sh
@@ -8,7 +8,7 @@ docker run -itd --privileged --name frr --network host petarmi/frr:6.0.2
 
 echo "3/5 Copying config files to FRR container"
 config_file=$(hostname)
-docker cp /home/configs/$config_file.conf frr:/home/device-config.conf
+docker cp /home/configs/${config_file}.conf frr:/home/device-config.conf
 
 echo "4/5 Setting configs to frr.conf"
 docker exec frr bash -c "cp /home/device-config.conf /etc/frr/frr.conf"
--- containers/playmaker/scripts/topoSetup.sh
+++ containers/playmaker/scripts/topoSetup.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
-while IFS=, read -r col1 col2
-do
-    docker exec ${col1} "/home/scripts/setupFRR.sh"
-done < /home/net_state/dockers.csv
+readonly DOCKER_DATA=/home/net_state/dockers.csv
+readonly FRR_SETUP_SCRIPT=/home/scripts/setupFRR.sh
 
+readonly L_GREEN='\033[1;32m'
+readonly GREEN='\033[0;32m'
+readonly CYAN='\033[0;36m'
+readonly RED='\033[0;31m'
+readonly NC='\033[0m' # No Color
+
+printf "${L_GREEN}##### Setup all Layer 2 containers${NC}\n"
+
+pids=()
+
+while IFS=, read -r name type
+do
+    printf "${GREEN}## Configuring container ${name}${NC}\n"
+    docker exec ${name} ${FRR_SETUP_SCRIPT} &
+    pids+=($!)
+done < ${DOCKER_DATA}
+
+echo "${#pids[@]}"
+echo "Processing..."
+
+# wait for all containers to finish setting up
+for pid in ${pids[*]}; do
+    wait $pid
+done
+
+printf "${L_GREEN}### Done ${NC}\n"
 
 #awk -F, '
 #{
