352b37be3dcd101ceab497bbcbe3724c85fb1cb6
PetarMI <petar2394@gmail.com>
2019-04-08 23:21:29 +0200

    Script for rebuilding docker image on VMs

--- infra/playmaker/vm_comms/local_vm_env.sh
+++ infra/playmaker/vm_comms/local_vm_env.sh
@@ -8,20 +8,23 @@
 usage="Script for checking and setting up the VM environment
 
 where:
+    -i     check if everything is installed
     -c     clean VM dirs
     -d     setup dir structure
     -p     rebuild the Layer 2 image
     -v     view Dir structure
     -h     show this help text"
 
+FLAG_installed=0
 FLAG_clean=0
 FLAG_dirs=0
 FLAG_build_phynet=0
 FLAG_view=0
 
-while getopts "cdovh" option
+while getopts "icdpvh" option
 do
     case "${option}" in
+        i) FLAG_installed=1;;
         c) FLAG_clean=1;;
         d) FLAG_dirs=1;;
         p) FLAG_build_phynet=1;;
@@ -48,6 +51,10 @@ readonly MACHINE="osboxes@localhost"
 readonly GREEN='\033[0;32m'
 readonly RED='\033[0;31m'
 readonly NC='\033[0m' # No Color
+
+function check_installed {
+    printf "${RED}Checking prerequisites not implemented${NC}\n"
+}
 
 function clean {
     while IFS=, read -r idx port role
@@ -78,14 +85,15 @@ EOF
     printf "${GREEN}/phynet/scripts${NC} - store the Layer 2 scripts\n"
     printf "${GREEN}/device_configs${NC} - store config files for all topology devices\n"
 }
-
+#ls -lR
 function view {
     while IFS=, read -r idx port role
     do
         if [[ ${role} == "manager" ]]
         then
 ssh -T -p ${port} ${MACHINE} << EOF
-    ls -lR
+    cd ${VM_WORK_DIR}/${VM_DOCKER_DIR}
+    ls
 EOF
             break
         fi
@@ -96,8 +104,24 @@ EOF
 # Rebuild the Layer 2 Docker image on every VM
 #######################################
 function rebuild_docker {
-    printf "${RED}Building Layer 2 container not implemented${NC}\n"
+    while IFS=, read -r idx port role
+    do
+        echo "## Rebuilding L2 image on VM ${idx} ##"
+ssh -T -p ${port} ${MACHINE} <<- 'EOF'
+    docker images | grep phynet | awk '{print $3}' | xargs docker rmi
+    cd phynet/
+    docker build --rm -t phynet:latest .
+EOF
+    done < ${CONF_FILE}
 }
+
+#######################################
+# Actual script logic
+#######################################
+if [[ ${FLAG_installed} == 1 ]]; then
+    echo "##### Verifying installation prerequisites #####"
+    check_installed
+fi
 
 if [[ ${FLAG_clean} == 1 ]]; then
     echo "##### Cleaning VM directories #####"
@@ -107,6 +131,7 @@ fi
 if [[ ${FLAG_dirs} == 1 ]]; then
     echo "##### Building VM dir structure #####"
     setup_dir_structure
+    check_installed
 fi
 
 if [[ ${FLAG_view} == 1 ]]; then
