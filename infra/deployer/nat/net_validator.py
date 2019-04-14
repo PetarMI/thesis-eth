cce4b9cc00212db7e39c4fa13f59e56bd6f2a6a1
PetarMI <petar2394@gmail.com>
2019-04-14 15:00:24 +0200

    More tests

--- infra/deployer/nat/net_validator.py
+++ infra/deployer/nat/net_validator.py
@@ -2,11 +2,13 @@ import ipaddress
 import file_reader as fr
 
 
+# @Tested
 def validate_subnets(subnets: dict):
     for subnet in subnets.values():
         validate_subnet(subnet)
 
 
+# @Tested above
 def validate_subnet(subnet: str):
     try:
         parsed_subnet = ipaddress.IPv4Network(subnet, strict=True)
@@ -21,5 +23,23 @@ def validate_subnet(subnet: str):
         raise
 
 
-# some_nets = fr.read_sim_subnets("toy")
-# validate_subnets(some_nets)
+def validate_interfaces(interfaces: dict):
+    for config in interfaces.values():
+        for ip in config.values():
+            validate_interface(ip)
+
+
+def validate_interface(interface: str):
+    try:
+        parsed_ip = ipaddress.IPv4Interface(interface)
+        if (parsed_ip.network.prefixlen == 32):
+            raise ValueError("No prefix for subnet: {}".format(interface))
+    except ipaddress.AddressValueError:
+        raise
+    except ipaddress.NetmaskValueError as err:
+        err.args = ("Invalid netmask: {}".format(interface),)
+        raise
+
+
+#ifaces = fr.read_orig_ifaces("toy")
+#validate_interfaces(ifaces)
--- infra/deployer/nat/tests/test_validation.py
+++ infra/deployer/nat/tests/test_validation.py
@@ -3,7 +3,10 @@ import pytest
 import net_validator
 
 
-def test_subnets_success():
+# #############################################################################
+# ############################# SUBNETS #######################################
+# #############################################################################
+def test_valid_subnets_success():
     subnets = {
         "net1": "10.0.1.0/24",
         "net2": "10.0.2.0/24",
@@ -15,7 +18,7 @@ def test_subnets_success():
     net_validator.validate_subnets(subnets)
 
 
-def test_subnets_fail_invalid():
+def test_valid_subnets_fail_invalid_ip():
     with pytest.raises(ipaddress.AddressValueError):
         subnets = {
             "net1": "10.0.1.0/24",
@@ -28,7 +31,7 @@ def test_subnets_fail_invalid():
         net_validator.validate_subnets(subnets)
 
 
-def test_subnets_fail_empty():
+def test_valid_subnets_fail_empty():
     with pytest.raises(ipaddress.AddressValueError):
         subnets = {
             "net1": "10.0.1.0/24",
@@ -41,7 +44,7 @@ def test_subnets_fail_empty():
         net_validator.validate_subnets(subnets)
 
 
-def test_subnets_fail_netmask():
+def test_valid_subnets_fail_netmask():
     with pytest.raises(ipaddress.NetmaskValueError, match="Invalid netmask: 10.0.5.0/224"):
         subnets = {
             "net1": "10.0.1.0/24",
@@ -54,7 +57,7 @@ def test_subnets_fail_netmask():
         net_validator.validate_subnets(subnets)
 
 
-def test_subnets_fail_netmask_empty():
+def test_valid_subnets_fail_netmask_empty():
     with pytest.raises(ipaddress.NetmaskValueError, match="Invalid netmask: 10.0.5.0/"):
         subnets = {
             "net1": "10.0.1.0/24",
@@ -67,7 +70,7 @@ def test_subnets_fail_netmask_empty():
         net_validator.validate_subnets(subnets)
 
 
-def test_subnets_fail_no_netmask():
+def test_valid_subnets_fail_no_netmask():
     with pytest.raises(ValueError, match="No prefix for subnet: 10.0.1.0"):
         subnets = {
             "net1": "10.0.1.0",
@@ -80,7 +83,7 @@ def test_subnets_fail_no_netmask():
         net_validator.validate_subnets(subnets)
 
 
-def test_subnets_fail_hostbits():
+def test_valid_subnets_fail_hostbits():
     with pytest.raises(ValueError, match="10.0.2.0/14 has host bits set"):
         subnets = {
             "net1": "10.0.1.0/24",
@@ -93,7 +96,7 @@ def test_subnets_fail_hostbits():
         net_validator.validate_subnets(subnets)
 
 
-def test_subnets_fail_hostbits2():
+def test_valid_subnets_fail_hostbits2():
     with pytest.raises(ValueError, match="10.0.3.3/24 has host bits set"):
         subnets = {
             "net1": "10.0.1.0/24",
@@ -106,7 +109,7 @@ def test_subnets_fail_hostbits2():
         net_validator.validate_subnets(subnets)
 
 
-def test_subnets_fail_mixed():
+def test_valid_subnets_fail_mixed():
     with pytest.raises(ipaddress.NetmaskValueError, match="Invalid netmask: 10.0.3.0/"):
         subnets = {
             "net1": "10.0.1.0/24",
@@ -117,3 +120,166 @@ def test_subnets_fail_mixed():
         }
 
         net_validator.validate_subnets(subnets)
+
+
+# #############################################################################
+# ########################## INTERFACES #######################################
+# #############################################################################
+def test_valid_ifaces_success():
+    interfaces = {
+        "topo-r01": {
+            "eth0": "10.0.1.3/24",
+            "eth1": "10.0.2.3/24",
+            "eth2": "10.0.3.2/24",
+        },
+        "topo-r02": {
+            "eth5": "10.0.2.2/24",
+            "eth4": "10.0.5.1/24",
+        },
+        "topo-r03": {
+            "eth2": "10.0.1.4/24",
+            "eth6": "10.0.2.4/24",
+            "eth3": "10.0.4.2/24",
+            "eth0": "10.0.5.5/24"
+        }
+    }
+
+    net_validator.validate_interfaces(interfaces)
+
+
+def test_valid_ifaces_fail_invalid():
+    with pytest.raises(ipaddress.AddressValueError):
+        interfaces = {
+            "topo-r01": {
+                "eth0": "10.0.1.3/24",
+                "eth1": "10.0.2.3/24",
+                "eth2": "10.0.3.2/24",
+            },
+            "topo-r02": {
+                "eth5": "10.0.2.2/24",
+                "eth4": "10.0.514.1/24",
+            },
+            "topo-r03": {
+                "eth2": "10.0.1.4/24",
+                "eth6": "10.0.2.4/24",
+                "eth3": "10.0.4.2/24",
+                "eth0": "10.0.5.5/24"
+            }
+        }
+
+        net_validator.validate_interfaces(interfaces)
+
+
+def test_valid_ifaces_fail_empty():
+    with pytest.raises(ipaddress.AddressValueError):
+        interfaces = {
+            "topo-r01": {
+                "eth0": "10.0.1.3/24",
+                "eth1": "10.0.2.3/24",
+                "eth2": "10.0.3.2/24",
+            },
+            "topo-r02": {
+                "eth5": "10.0.2.2/24",
+                "eth4": "10.0.5.1/24",
+            },
+            "topo-r03": {
+                "eth2": "",
+                "eth6": "10.0.2.4/24",
+                "eth3": "10.0.4.2/24",
+                "eth0": "10.0.5.5/24"
+            }
+        }
+
+        net_validator.validate_interfaces(interfaces)
+
+
+def test_valid_ifaces_fail_netmask():
+    with pytest.raises(ipaddress.NetmaskValueError, match="Invalid netmask: 10.0.5.1/224"):
+        interfaces = {
+            "topo-r01": {
+                "eth0": "10.0.1.3/24",
+                "eth1": "10.0.2.3/24",
+                "eth2": "10.0.3.2/24",
+            },
+            "topo-r02": {
+                "eth5": "10.0.2.2/24",
+                "eth4": "10.0.5.1/224",
+            },
+            "topo-r03": {
+                "eth2": "10.0.1.4/24",
+                "eth6": "10.0.2.4/24",
+                "eth3": "10.0.4.2/24",
+                "eth0": "10.0.5.5/24"
+            }
+        }
+
+        net_validator.validate_interfaces(interfaces)
+
+
+def test_valid_ifaces_fail_netmask_empty():
+    with pytest.raises(ipaddress.NetmaskValueError, match="Invalid netmask: 10.0.3.2/"):
+        interfaces = {
+            "topo-r01": {
+                "eth0": "10.0.1.3/24",
+                "eth1": "10.0.2.3/24",
+                "eth2": "10.0.3.2/",
+            },
+            "topo-r02": {
+                "eth5": "10.0.2.2/24",
+                "eth4": "10.0.5.1/224",
+            },
+            "topo-r03": {
+                "eth2": "10.0.1.4/24",
+                "eth6": "10.0.2.4/24",
+                "eth3": "10.0.4.2/24",
+                "eth0": "10.0.5.5/24"
+            }
+        }
+
+        net_validator.validate_interfaces(interfaces)
+
+
+def test_valid_ifaces_fail_no_netmask():
+    with pytest.raises(ValueError, match="No prefix for subnet: 10.0.2.2"):
+        interfaces = {
+            "topo-r01": {
+                "eth0": "10.0.1.3/24",
+                "eth1": "10.0.2.3/24",
+                "eth2": "10.0.3.2/14",
+            },
+            "topo-r02": {
+                "eth5": "10.0.2.2",
+                "eth4": "10.0.5.1/24",
+            },
+            "topo-r03": {
+                "eth2": "10.0.1.4/24",
+                "eth6": "10.0.2.422/24",
+                "eth3": "10.0.4.2/24",
+                "eth0": "10.0.5.5/24"
+            }
+        }
+
+        net_validator.validate_interfaces(interfaces)
+
+
+def test_valid_ifaces_fail_mixed():
+    with pytest.raises(ipaddress.NetmaskValueError, match="Invalid netmask: 10.0.3.2/"):
+        interfaces = {
+            "topo-r01": {
+                "eth0": "10.0.1.3/24",
+                "eth1": "10.0.2.3/24",
+                "eth2": "10.0.3.2/",
+            },
+            "topo-r02": {
+                "eth5": "10.0.2.2",
+                "eth4": "10.0.5.1/24",
+            },
+            "topo-r03": {
+                "eth2": "10.0.1.4/24",
+                "eth6": "10.0.2.422/24",
+                "eth3": "10.0.4.2/24",
+                "eth0": "10.0.5.5/24"
+            }
+        }
+
+        net_validator.validate_interfaces(interfaces)
