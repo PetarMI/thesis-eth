0d0896480ed6f35abd644258f63b8b7ee4b95cee
PetarMI <petar2394@gmail.com>
2019-04-29 15:38:47 +0200

    Reachability checker implementation

--- fuzzer/controllers/file_reader.py
+++ fuzzer/controllers/file_reader.py
@@ -1,6 +1,7 @@
 import csv
+import ipaddress
 import json
-from fuzzer.controllers import constants_controller as const
+import constants_controller as const
 
 
 def read_topo() -> dict:
@@ -10,22 +11,45 @@ def read_topo() -> dict:
     return topo
 
 
-def read_properties() -> dict:
+def read_properties(prop_type: str) -> dict:
 
     with open(const.PROPERTIES_FILE) as properties_file:
         properties = json.load(properties_file)
 
-    return properties
+    return properties[prop_type]
 
 
 def read_nat_ips() -> dict:
-    matched_ips = const.IP_NAT_FILE
-    return read_dev_csv(matched_ips)
+    nat_ips_file = const.IP_NAT_FILE
+
+    nat_ips = {}
+
+    with open(nat_ips_file, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        for row in csv_reader:
+            casted_orig_ip = ipaddress.IPv4Interface(row[1])
+            nat_ips.setdefault(row[0], {}).update({
+                casted_orig_ip: row[2]
+            })
+
+    return nat_ips
 
 
 def read_vm_info() -> dict:
     vm_file = const.VM_FILE
-    return read_dev_csv(vm_file)
+    vm_dict = {}
+
+    with open(vm_file, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        for row in csv_reader:
+            vm_dict.setdefault(row[0], {}).update({
+                "ip": row[1],
+                "role": row[2]
+            })
+
+    return vm_dict
 
 
 def read_dev_csv(filepath: str) -> dict:
--- fuzzer/controllers/reachability_parser.py
+++ fuzzer/controllers/reachability_parser.py
@@ -1,1 +1,70 @@
-from fuzzer.controllers import file_reader as fr
+import ipaddress
+import file_reader as fr
+
+
+def main():
+    reach_properties: dict = fr.read_properties("reachability")
+    vms: dict = fr.read_vm_info()
+    topo: dict = fr.read_topo()
+    nat_ips: dict = fr.read_nat_ips()
+
+    properties: list = parse_properties(reach_properties, vms, topo, nat_ips)
+
+
+def parse_properties(raw_properties, vms, topo, nat_ips) -> list:
+    properties = []
+
+    for raw_property in raw_properties:
+        property = {}
+        property["vm_ip"] = get_vm_ip(raw_property, topo["containers"], vms)
+        property["container_name"] = get_container_name(topo["meta"]["name"],
+                                                        raw_property["src"])
+        property["dest_ip"] = get_nat_ip(raw_property["dest"], nat_ips)
+
+        properties.append(property)
+
+    print(properties)
+    return properties
+
+
+def get_vm_ip(property: dict, topo_containers: dict, vms: dict) -> str:
+    """ Find the VM IP on which the container is running """
+
+    src_name = property["src"]
+    vm_id = None
+
+    for container in topo_containers:
+        if src_name == container["name"]:
+            vm_id = container["vm"]
+
+    if vm_id is None:
+        raise ValueError("Property src container not in topo file {}".format(src_name))
+
+    vm = vms.get(vm_id, None)
+
+    if vm is None:
+        raise ValueError("No running VM with ID {}".format(vm_id))
+
+    return vm["ip"]
+
+
+# TODO this is dealing with reachability addresses specified without netmask
+# function wouldn't need to cast if the reachability address was given one
+def get_nat_ip(dest_ip: str, nat_ips: dict) -> str:
+    casted_dest_ip = ipaddress.IPv4Address(dest_ip)
+
+    for cont_ips in nat_ips.values():
+        for orig_iface in cont_ips.keys():
+            casted_orig_ip = orig_iface.ip
+            if casted_dest_ip == casted_orig_ip:
+                return cont_ips[orig_iface]
+
+    raise ValueError("Original IP {} not in NAT logs".format(dest_ip))
+
+
+def get_container_name(topo_name: str, container: str) -> str:
+    return "{}-{}".format(topo_name, container)
+
+
+if __name__ == '__main__':
+    main()
