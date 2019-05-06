9607604fabc1bc23aafcd8efc9e769ee3ea86da9
PetarMI <petar2394@gmail.com>
2019-05-06 17:54:51 +0200

    Started interface config generation

--- synth/cisco/config_generator.py
+++ synth/cisco/config_generator.py
@@ -6,22 +6,47 @@ def generate_config_files(cisco_configs, topo_name):
     frr_configs = dict()
 
     for hostname, host_configs in cisco_configs.items():
-        host_configs = []
+        host_instr = []
 
-        host_configs.extend(generate_meta(hostname))
-        host_configs.extend(generate_file_end())
+        host_instr.extend(generate_meta(hostname))
+        host_instr.extend(generate_interfaces(host_configs["interfaces"]))
+        host_instr.extend(generate_router_section())
+        host_instr.extend(generate_file_end())
 
-        frr_configs.update({hostname: host_configs})
+        frr_configs.update({hostname: host_instr})
 
     fw.write_config_files(topo_name, frr_configs)
 
 
-def generate_meta(hostname):
+def generate_interfaces(host_interfaces: list) -> list:
+    all_ifaces = []
+
+    for interface in host_interfaces:
+        all_ifaces.extend(generate_interface_instr(interface))
+
+    return all_ifaces
+
+
+def generate_interface_instr(interface: dict) -> list:
+    instr = []
+
+    instr.append("interface {}".format(interface["name"]))
+    instr.append(const.CMD_END_SEC)
+
+    return instr
+
+
+def generate_router_section() -> list:
+    # TODO
+    return []
+
+
+def generate_meta(hostname) -> list:
     meta = [const.CMD_FRR_VER, const.CMD_FRR_DEF, const.CMD_HOST.format(hostname),
             const.CMD_INTEGRATED, const.CMD_END_SEC]
 
     return meta
 
 
-def generate_file_end():
+def generate_file_end() -> list:
     return ["line vty", "!"]
