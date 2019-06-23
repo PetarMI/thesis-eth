e1cca31c8eb9088b10c35a7a44822d2ddb14714e
PetarMI <petar2394@gmail.com>
2019-06-23 17:22:47 +0200

    Implemented heuristic search strategy

--- fuzzer/controllers/search_strategies.py
+++ fuzzer/controllers/search_strategies.py
@@ -13,6 +13,7 @@ Public functions:
 """
 
 from itertools import combinations
+from collections import OrderedDict
 
 
 # @Tested
@@ -37,9 +38,17 @@ def dfs(max_depth: int, nets: list) -> list:
     return statespace
 
 
+# @Tested
 def heuristic(max_depth: int, links: list, property_links: list) -> list:
     """ Makes a search plan based on a heuristic """
-    raise ValueError("Not implemented")
+    heuristic_subplan: list = dfs(max_depth, property_links)
+    full_dfs_plan: list = dfs(max_depth, links)
+
+    pre_validate_heuristic_gen(heuristic_subplan, full_dfs_plan)
+    heuristic_plan: list = gen_full_heuristic(heuristic_subplan, full_dfs_plan)
+    post_validate_heuristic_gen(heuristic_plan, full_dfs_plan)
+
+    return heuristic_plan
 
 
 # @Tested as part of calling function
@@ -63,3 +72,32 @@ def dfs_aux(parent_state: tuple, nets: list, depth: int, max_depth: int) -> list
         search_plan.extend(child_plan)
 
     return search_plan
+
+
+# @Tested
+def gen_full_heuristic(subplan: list, full_plan: list) -> list:
+    lookup_subplan = OrderedDict.fromkeys(subplan)
+    heuristic_plan = subplan.copy()
+
+    for state in full_plan:
+        if state not in lookup_subplan:
+            heuristic_plan.append(state)
+
+    return heuristic_plan
+
+
+# @Tested
+def pre_validate_heuristic_gen(heuristic_plan: list, full_plan: list):
+    if not heuristic_plan:
+        raise ValueError("Empty heuristic plan")
+
+    if not set(heuristic_plan).issubset(set(full_plan)):
+        raise ValueError("Heuristic plan is not subset of the full plan")
+
+
+# @Tested
+def post_validate_heuristic_gen(heuristic_plan: list, dfs_plan: list):
+    if len(heuristic_plan) != len(dfs_plan):
+        raise ValueError("Heuristic plan expected length - {} vs real {}".format(
+            len(heuristic_plan), len(dfs_plan)
+        ))
--- fuzzer/controllers/tests/test_search_strategies.py
+++ fuzzer/controllers/tests/test_search_strategies.py
@@ -1,3 +1,4 @@
+import pytest
 import search_strategies as ss
 
 
@@ -128,3 +129,129 @@ def test_dfs_higher_depth():
     ]
 
     assert(res_plan == expected_plan)
+
+
+###############################################################################
+# ############################ HEURISTIC ######################################
+###############################################################################
+def test_complete_heuristic():
+    property_links = ['b', 'd']
+    all_links = ['a', 'b', 'c', 'd']
+
+    res_plan = ss.heuristic(3, all_links, property_links)
+    expected_plan = [
+        (), ('b',), ('b', 'd'), ('d',),
+        ('a',), ('a', 'b'), ('a', 'b', 'c'), ('a', 'b', 'd'),
+        ('a', 'c'), ('a', 'c', 'd'), ('a', 'd'),
+        ('b', 'c'), ('b', 'c', 'd'), ('c',), ('c', 'd')
+    ]
+
+    assert(res_plan == expected_plan)
+
+
+def test_complete_heuristic_longer():
+    property_links = ['a', 'b', 'd']
+    all_links = ['a', 'b', 'c', 'd']
+
+    res_plan = ss.heuristic(3, all_links, property_links)
+    expected_plan = [
+        (), ('a',), ('a', 'b'), ('a', 'b', 'd'),
+        ('a', 'd'), ('b',), ('b', 'd'), ('d',),
+        ('a', 'b', 'c'), ('a', 'c'), ('a', 'c', 'd'),
+        ('b', 'c'), ('b', 'c', 'd'), ('c',), ('c', 'd')
+    ]
+
+    assert(res_plan == expected_plan)
+
+
+def test_complete_heuristic_even_longer():
+    property_links = ['a', 'c', 'd']
+    all_links = ['a', 'b', 'c', 'd']
+
+    res_plan = ss.heuristic(4, all_links, property_links)
+    expected_plan = [
+        (), ('a',), ('a', 'c'), ('a', 'c', 'd'), ('a', 'd'), ('c',),
+        ('c', 'd'), ('d',), ('a', 'b'), ('a', 'b', 'c'),
+        ('a', 'b', 'c', 'd'), ('a', 'b', 'd'), ('b',), ('b', 'c'),
+        ('b', 'c', 'd'), ('b', 'd'),
+
+    ]
+
+    assert(res_plan == expected_plan)
+
+
+def test_gen_full_heuristic():
+    subplan = [('a',), ('a', 'b'), ('b',)]
+    full_plan = [('a',), ('a', 'b'), ('a', 'b', 'c'), ('a', 'c'), ('b',),
+                 ('b', 'c'), ('c',)]
+
+    res_plan = ss.gen_full_heuristic(subplan, full_plan)
+    expected_plan = [('a',), ('a', 'b'), ('b',),
+                     ('a', 'b', 'c'), ('a', 'c'), ('b', 'c'), ('c',)]
+
+    assert(res_plan == expected_plan)
+
+
+def test_gen_full_heuristic_no_overlap():
+    subplan = [('a',), ('a', 'b'), ('b',)]
+    full_plan = [('a', 'b', 'c'), ('a', 'c'), ('b', 'c'), ('c',)]
+
+    res_plan = ss.gen_full_heuristic(subplan, full_plan)
+    expected_plan = [('a',), ('a', 'b'), ('b',),
+                     ('a', 'b', 'c'), ('a', 'c'), ('b', 'c'), ('c',)]
+
+    assert(res_plan == expected_plan)
+
+
+def test_gen_full_heuristic_full_overlap():
+    subplan = [('a', 'c'), ('a',), ('a', 'b', 'c'), ('a', 'b'), ('b',)]
+    full_plan = [('a',), ('a', 'b'), ('a', 'b', 'c'), ('a', 'c'), ('b',)]
+
+    res_plan = ss.gen_full_heuristic(subplan, full_plan)
+    expected_plan = [('a', 'c'), ('a',), ('a', 'b', 'c'), ('a', 'b'), ('b',)]
+
+    assert(res_plan == expected_plan)
+
+
+###############################################################################
+# ###################### HEURISTIC VALIDATION #################################
+###############################################################################
+def test_heuristic_pre_validation():
+    subplan = [('a',), ('a', 'b'), ('b',)]
+    full_plan = [('a',), ('a', 'b'), ('a', 'b', 'c'), ('a', 'c'), ('b',),
+                 ('b', 'c'), ('c',)]
+
+    ss.pre_validate_heuristic_gen(subplan, full_plan)
+
+
+def test_heuristic_pre_validation_not_subset():
+    with pytest.raises(ValueError, match="Heuristic plan is not subset of the full plan"):
+        subplan = [('a',), ('a', 'b'), ('b',), ('x',)]
+        full_plan = [('a',), ('a', 'b'), ('a', 'b', 'c'), ('a', 'c'), ('b',),
+                     ('b', 'c'), ('c',)]
+
+        ss.pre_validate_heuristic_gen(subplan, full_plan)
+
+
+def test_heuristic_pre_validation_empty_plan():
+    with pytest.raises(ValueError, match="Empty heuristic plan"):
+        subplan = []
+        full_plan = [('a',), ('a', 'b'), ('a', 'b', 'c'), ('a', 'c'), ('b',),
+                     ('b', 'c'), ('c',)]
+
+        ss.pre_validate_heuristic_gen(subplan, full_plan)
+
+
+def test_heuristic_pos_validation_ok():
+    heuristic_plan = [('a',), ('a', 'b'), ('b',)]
+    full_plan = [('a',), ('a', 'b'), ('a', 'b', 'c')]
+
+    ss.post_validate_heuristic_gen(heuristic_plan, full_plan)
+
+
+def test_heuristic_pos_validation_not_ok():
+    with pytest.raises(ValueError, match="Heuristic plan expected length - 2 vs real 3"):
+        heuristic_plan = [('a',), ('a', 'b')]
+        full_plan = [('a',), ('a', 'b'), ('a', 'b', 'c')]
+
+        ss.post_validate_heuristic_gen(heuristic_plan, full_plan)
