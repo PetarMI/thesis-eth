4996ea388cc579bd4cd013866aed6852ac5877cc
PetarMI <petar2394@gmail.com>
2019-06-30 16:40:53 +0200

    Shutting down only interfaces only on one container

--- fuzzer/transitions/InstructionGenerator.py
+++ fuzzer/transitions/InstructionGenerator.py
@@ -1,3 +1,4 @@
+import random
 from fuzzer.common.FuzzData import FuzzData
 from fuzzer.common import constants_fuzzer as const
 
@@ -5,6 +6,7 @@ from fuzzer.common import constants_fuzzer as const
 class InstructionGenerator:
     def __init__(self, fuzz_data: FuzzData):
         self.fuzz_data = fuzz_data
+        self.link2dev = dict()
 
     def gen_transition_instructions(self, link_changes: dict) -> dict:
         instructions = {}
@@ -13,27 +15,22 @@ class InstructionGenerator:
 
         for op_type, links in link_changes.items():
             for link in links:
-                link_instructions: list = self.get_link_instructions(op_type, link)
-                instructions.setdefault(op_type, []).extend(link_instructions)
+                link_instruction: dict = self.get_link_instruction(op_type, link)
+                instructions.setdefault(op_type, []).append(link_instruction)
 
         return instructions
 
-    def get_link_instructions(self, op_type: str, link: str) -> list:
-        instructions = []
-        link_containers: list = self.fuzz_data.find_network_devices(link)
+    def get_link_instruction(self, op_type: str, link: str) -> dict:
+        dev = self.pick_link_device(link, op_type)
+        dev_instr = dict()
 
-        for dev in link_containers:
-            dev_instr = dict()
+        dev_instr["link"] = link
+        dev_instr["op_type"] = op_type
+        dev_instr["vm"] = self.fuzz_data.find_container_vm(dev)
+        dev_instr["container"] = dev
+        dev_instr["iface"] = self.fuzz_data.find_network_interface(dev, link)
 
-            dev_instr["link"] = link
-            dev_instr["op_type"] = op_type
-            dev_instr["vm"] = self.fuzz_data.find_container_vm(dev)
-            dev_instr["container"] = dev
-            dev_instr["iface"] = self.fuzz_data.find_network_interface(dev, link)
-
-            instructions.append(dev_instr)
-
-        return instructions
+        return dev_instr
 
     def get_transition_data(self, link_changes: dict):
         trans_data = dict()
@@ -56,11 +53,33 @@ class InstructionGenerator:
         affected_containers = []
 
         for net_name in networks:
-            # net_name: str = self.fuzz_data.get_sim_net_name(net_ip)
-            net_devices: list = self.fuzz_data.find_network_devices(net_name)
+            net_device: str = self.pick_link_device(net_name, const.RESTORE)
 
-            affected_containers.extend(net_devices)
+            affected_containers.append(net_device)
 
-        affected_containers = list(set(affected_containers))
+        return list(set(affected_containers))
 
-        return affected_containers
+    def pick_link_device(self, link: str, op_type: str) -> str:
+        dev: str = self.link2dev.get(link, None)
+
+        if op_type == const.DROP:
+            check_none(dev, True, "Device {} for link {} exists in logs"
+                       .format(dev, link))
+            link_containers: list = self.fuzz_data.find_network_devices(link)
+            dev = random.choice(link_containers)
+            self.link2dev.update({link: dev})
+        elif op_type == const.RESTORE:
+            check_none(dev, False, "Device {} for link {} doesn't exists in logs"
+                       .format(dev, link))
+
+        return dev
+
+
+def check_none(item, expected, msg):
+    res = False
+
+    if item is None:
+        res = True
+
+    if res != expected:
+        raise ValueError(msg)
