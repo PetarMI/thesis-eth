35080e1c3d0e81dab45290ffd4059938107cdc42
PetarMI <petar2394@gmail.com>
2019-07-15 21:30:59 +0200

    Finished updating FIB

--- fuzzer/controllers/fib.py
+++ fuzzer/controllers/fib.py
@@ -6,14 +6,42 @@ from fuzzer.common import constants_fuzzer as const
 
 class Fib:
     def __init__(self, fuzz_data: FuzzData):
-        self.fib = dict()
         self.dev2vm = fuzz_data.get_dev2vm()
+        self.fib = self.prepare_fib()
 
     def update_fib(self):
         for dev, vm in self.dev2vm.items():
-            fib: str = exec_get_fib(vm, dev)
-            parsed_fib: dict = json.loads(fib)
-            print(parsed_fib.keys())
+            raw_fib: str = exec_get_fib(vm, dev)
+
+            dev_fib: dict = self.parse_device_fib(raw_fib)
+            self.fib.update({dev: dev_fib})
+
+        print(json.dumps(self.fib, indent=4))
+
+    @staticmethod
+    def parse_device_fib(raw_fib: str) -> dict:
+        parsed_fib: dict = json.loads(raw_fib)
+        dev_fib = dict()
+
+        for dest_network, prefix_data in parsed_fib.items():
+            next_hops = []
+            pdata = validate_prefix_data(prefix_data)
+  
+            if pdata["protocol"] == "ospf":
+                for next_hop in pdata["nexthops"]:
+                    next_hops.append(next_hop["ip"])
+            
+            dev_fib.update({dest_network: next_hops})
+
+        return dev_fib
+
+    def prepare_fib(self):
+        fib = dict()
+
+        for dev in self.dev2vm.keys():
+            fib.update({dev: dict()})
+
+        return fib
 
 
 def exec_get_fib(vm_ip, src_dev) -> str:
@@ -21,3 +49,11 @@ def exec_get_fib(vm_ip, src_dev) -> str:
                             stdout=subprocess.PIPE)
 
     return result.stdout.decode('utf-8')
+
+
+def validate_prefix_data(prefix_data: list) -> dict:
+    if len(prefix_data) > 1:
+        raise ValueError("Prefix data for some fib is larger than 1")
+    
+    return prefix_data[0]
+
